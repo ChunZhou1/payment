7cf3d1f41b714075cc7fde49b11be9995b6919d3
Jack.study <njzhch@163.com>
2021-03-14 17:10:03 -0400

    payment first

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+// This is a sample test API key. Sign in to see examples pre-filled with your key.
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+app.use(express.static("."));
+app.use(express.json());
+
+var customerId = "cus_J7FWa77ECE306y"; //for test*/
+/*var customerId = "";*/
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  paymentIntentData = {
+    amount: calculateOrderAmount(items),
+    currency: "usd"
+  };
+
+  var paymentMethodsId = "";
+
+  if (customerId != "") {
+    //get paymentmethod id for client use
+    var paymentMethods1 = await stripe.paymentMethods.list({
+      customer: customerId,
+      type: "card"
+    });
+
+    console.log(paymentMethods1);
+
+    paymentMethodsId = paymentMethods1.data[0].id;
+
+    paymentIntentData.customer = customerId;
+    paymentIntentData.payment_method = paymentMethodsId;
+
+    //we cannot use it
+    /*paymentIntentData.confirm = "true";*/
+  } else {
+    const customer = await stripe.customers.create();
+    paymentIntentData.customer = customer.id;
+    console.log(customer.id);
+  }
+
+  paymentIntentData.setup_future_usage = "off_session";
+
+  const paymentIntent = await stripe.paymentIntents.create(paymentIntentData);
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    payMethodsId: paymentMethodsId
+  });
+});
+
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
